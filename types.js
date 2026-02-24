0816c0a16f0cd4000a989c26539f5e7afd1513e5
Kaif6361 <gayasuddingayas5@gmail.com>
2026-02-24 15:23:17 +0530

    Add files via upload

---
+++ types.js
@@ -0,0 +1,10 @@
+let objectExample = {
+    age:22
+};
+
+let arr = [1,2,3];
+console.log(typeof arr);
+console.log(objectExample);
+console.log(objectExample.name);
+console.log("0.1 + 0.2=",0.1+0.2);
+console.log(typeof 22);
