5356c6af693bf8f2f5a845b8b39f7fbb63b02934
Syed Kaif <gayasuddingayas5@gmail.com>
2026-02-26 21:38:33 +0530

    Add files via upload

--- types.js
+++ types.js
@@ -2,9 +2,34 @@ let objectExample = {
     age:22
 };
 
-let arr = [1,2,3];
-console.log(typeof arr);
+
 console.log(objectExample);
-console.log(objectExample.name);
+console.log(objectExample.age);
 console.log("0.1 + 0.2=",0.1+0.2);
 console.log(typeof 22);
+function declarationDemo () {
+
+    const greet = () => {
+      console.log("hello kaif");
+    };
+    greet();
+}
+declarationDemo();
+function add(a,b) {
+    return a+b;
+}
+console.log(add(5,10));
+
+const sayhello=function(){
+    console.log("Hello JavaScript");
+};
+sayhello();
+function processUser(name,callback) {
+    console.log("processing user:" + name);
+    callback();
+}
+function done () {
+    console.log("user processed successfully");
+
+}
+processUser("kaif",done);
